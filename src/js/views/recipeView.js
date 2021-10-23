564f975a1561b1e146ce5824551c0057accf711e
MrHamid-am <alimohamadihamidreza560@gmail.com>
2021-10-23 20:48:58 +0330

    Fix fractional issue *Remove fractional library

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -2,7 +2,6 @@ import View from "./View.js";
 
 // import icons from "../img/icons.svg"; // Parcel 1
 import icons from "url:../../img/icons.svg"; // Parcel 2
-import { Fraction } from "fractional";
 
 class RecipeView extends View {
     _parentElement = document.querySelector(".recipe");
@@ -134,9 +133,7 @@ class RecipeView extends View {
               <svg class="recipe__icon">
                 <use href="${icons}#icon-check"></use>
               </svg>
-              <div class="recipe__quantity">${
-                  ingredient.quantity ? new Fraction(ingredient.quantity) : ""
-              }</div>
+              <div class="recipe__quantity">${ingredient.quantity}</div>
               <div class="recipe__description">
                 <span class="recipe__unit">${ingredient.unit}</span>
                 ${ingredient.description}
